f5050adb421c413724a6e4793b34fdde0a8ff6af
Jessica Do <jessicanhu.do@gmail.com>
2026-01-16 22:04:44 -0600

    implement navigation

---
+++ components/Header.tsx
@@ -0,0 +1,44 @@
+"use client"; //turn this into client side rendered component
+
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+const Header = () => {
+  const pathname = usePathname(); //see which link is currently active
+  return (
+    <header>
+      <div className="main-container inner">
+        <Link href="/">
+          <Image src="logo.svg" alt="CryptoDash logo" width={132} height={40} />
+        </Link>
+
+        <nav>
+          <Link
+            href="/"
+            className={cn("nav-link", {
+              "is-active": pathname === "/",
+              "is-home": true,
+            })}
+          >
+            Home
+          </Link>
+
+          <p>Search Modal</p>
+
+          <Link
+            href="/coins"
+            className={cn("nav-link", {
+              "is-active": pathname === "/coins",
+            })}
+          >
+            All Coins
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
